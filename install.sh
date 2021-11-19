bd2eb9121e68ddcb5f9e191aabef1c56e5c7ce8c
CiciQ <ciciq128@gmail.com>
2021-11-19 18:58:42 +0800

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+files="zshrc aliases tmux.conf vimrc gitconfig"
+
+for file in $files; do
+    origin="$HOME/.$file"
+    [[ -r "$origin" ]] && [[ -f "$origin" ]] && mv "$origin" "$origin.old";
+    echo "move $origin to $origin.old"
+
+    file="$(pwd)/$file"
+    [[ -r "$file" ]] && [[ -f "$file" ]] && ln -s -T "$file" "$origin";
+    echo "link $file to $origin"
+done
